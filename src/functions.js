0d047403aa105abbb8a417bee37b64b84e1567c1
LeVan Smith <lsmith@acorns.com>
2023-07-18 14:15:37 -0700

    Add Compose exporting capability

--- src/functions.js
+++ src/functions.js
@@ -62,6 +62,83 @@ function isDigit(c) {
     return c >= '0' && c <= '9';
 }
 
+// Add the [indentationString] to all but the first line
+const createDocumentationComment = (text, indentationString) => text
+    .trim()
+    .split("\n")
+    .map((line, index) => ((index > 0) ? `${indentationString}` : ``) + `// ${line}`)
+    .join("\n")
+
+// Ensure integers are formatted as floating point values (e.g. 1 -> 1.0)
+const toFixed = (number) => (Number.isInteger(number)) ? number.toFixed(1) : number
+
+// Utility because the ternary expression is not supported in Pulsar
+const ternary = (predicate, trueCase, falseCase) => (predicate) ? trueCase : falseCase
+
+const groupBy = (values, key) => values
+    .map((value) => value.value)
+    .reduce((acc, value) => {
+        (acc[value[key]] = acc[value[key]] || []).push(value);
+        return acc;
+    }, {});
+
+const hexToHexARGB = (hexString) => {
+    let { alpha, red, green, blue } = {
+        alpha: hexString.substring(6, 2).toUpperCase(),
+        red: hexString.substring(0, 2).toUpperCase(),
+        green: hexString.substring(2, 2).toUpperCase(),
+        blue: hexString.substring(4, 2).toUpperCase(),
+    }
+    return {
+        alpha: alpha,
+        red: red,
+        green: green,
+        blue: blue,
+        full: `0x${alpha}${red}${green}${blue}`
+    }
+}
+
+const getBrushType = ({ type }) => {
+    switch (type) {
+        case "Linear": return "linearGradient"
+        case "Radial": return "radialGradient"
+        case "Angular": return "sweepGradient"
+        default: return ""
+    }
+}
+
+const getFontStyle = ({ subfamily }) => (subfamily.toLowerCase().includes("italic")) ? "Italic" : "Normal"
+
+const getFontWeight = ({ subfamily }) => {
+    let lowered = subfamily.toLowerCase()
+    if (lowered.includes("emi")) return "SemiBold"
+    else if (lowered.includes("heavy")) return "Black"
+    else if (lowered.includes("bold")) return "Bold"
+    else if (lowered.includes("medium")) return "Medium"
+    else if (lowered.includes("ult") && lowered.includes("light")) return "Thin"
+    else return "Normal"
+}
+
+const getTextDecoration = ({ textDecoration }) => {
+    switch (textDecoration) {
+        case "Underline":
+            return "Underline"
+        case "Strikethrough":
+            return "LineThrough"
+        default:
+            break;
+    }
+}
+
+Pulsar.registerFunction("createDocumentationComment", createDocumentationComment)
+Pulsar.registerFunction("toFixed", toFixed)
+Pulsar.registerFunction("ternary", ternary)
+Pulsar.registerFunction("groupBy", groupBy)
+Pulsar.registerFunction("hexToHexARGB", hexToHexARGB)
+Pulsar.registerFunction("getBrushType", getBrushType)
+Pulsar.registerFunction("getFontStyle", getFontStyle)
+Pulsar.registerFunction("getFontWeight", getFontWeight)
+Pulsar.registerFunction("getTextDecoration", getTextDecoration)
 Pulsar.registerFunction("indentMultilineText", indentMultilineText)
 Pulsar.registerFunction("createFullTokenGroupPath", createFullTokenGroupPath)
 Pulsar.registerFunction("arrayConcat", arrayConcat)
