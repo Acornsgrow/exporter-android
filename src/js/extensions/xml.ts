0795fe63207bf09f0e3f617b8d8edb60dd97e90d
LeVan Smith <lsmith@acorns.com>
2023-08-02 17:32:44 -0700

    Implement eslint and other tooling
    - Refactor to better styling

--- src/js/extensions/xml.ts
+++ src/js/extensions/xml.ts
@@ -1,34 +1,32 @@
-import { TokenGroup } from "@supernovaio/supernova-sdk";
-import { PulsarFunction, PulsarTransformer } from "../pulsar";
+import { TokenGroup } from '@supernovaio/supernova-sdk'
+import {
+  createPulsarFunction,
+  createPulsarTransformer,
+} from '../pulsar/registration/create-registration'
 
-const createFullTokenGroupPath: PulsarFunction = new PulsarFunction(
-    "createFullTokenGroupPath",
-    (group: TokenGroup) => (group.isRoot) ? [] : group.path.concat(group.name)
+export const createFullTokenGroupPath = createPulsarFunction(
+  'createFullTokenGroupPath',
+  (group: TokenGroup) => (group.isRoot ? [] : group.path.concat(group.name)),
 )
 
 // Add the [indentationString] to all but the first line
-const indentMultiline: PulsarTransformer = new PulsarTransformer(
-    "indentMultiline",
-    (text: string, indentation: string) => text.trim().split("\n").join(`\n${indentation}`)
+export const indentMultiline = createPulsarTransformer(
+  'indentMultiline',
+  (text: string, indentation: string) =>
+    text.trim().split('\n').join(`\n${indentation}`),
 )
 
-const groupFontsByFamily: PulsarFunction = new PulsarFunction(
-    "groupFontsByFamily",
-    (fonts: any) => {
-        let groupBy = (xs: any, key: string) => {
-            return xs.reduce(function(rv: any, x: any) {
-              (rv[x[key].toLowerCase()] = rv[x[key].toLowerCase()] || []).push(x);
-              return rv;
-            }, {});
-          };
-    
-        return groupBy(fonts, "family")
+export const groupFontsByFamily = createPulsarFunction(
+  'groupFontsByFamily',
+  (fonts: any) => {
+    const groupBy = (xs: any, key: string) => {
+      return xs.reduce(function (rv: any, x: any) {
+        rv[x[key].toLowerCase()] = rv[x[key].toLowerCase()] || []
+        rv[x[key].toLowerCase()].push(x)
+        return rv
+      }, {})
     }
+
+    return groupBy(fonts, 'family')
+  },
 )
-
-export default [
-    createFullTokenGroupPath,
-    indentMultiline,
-    groupFontsByFamily,
-]
-
--- src/js/pulsar/registration/PulsarRegistry.ts
+++ src/js/pulsar/registration/PulsarRegistry.ts
@@ -1,19 +1,13 @@
-// @ts-nocheck
-import { PulsarFunction } from "./PulsarFunction";
-import { PulsarPayload } from "./PulsarPayload";
-import { PulsarRegistration } from "./PulsarRegistration";
-import { PulsarTransformer } from "./PulsarTransformer";
+import { PulsarRegistration } from './types'
 
-export namespace PulsarRegistry {
-    export function register(registrations: PulsarRegistration[]) {
-        for (let registration of registrations) {
-            if (registration instanceof PulsarFunction) {
-                Pulsar.registerFunction(registration.name, registration.func);
-            } else if (registration instanceof PulsarTransformer) {
-                Pulsar.registerTransformer(registration.name, registration.func);
-            } else if (registration instanceof PulsarPayload) {
-                Pulsar.registerPayload(registration.name, registration.obj);
-            }
-        }
+export function register(registrations: PulsarRegistration[]) {
+  for (const registration of registrations) {
+    if (registration.registrationType === 'function') {
+      Pulsar.registerFunction(registration.name, registration.func)
+    } else if (registration.registrationType === 'transformer') {
+      Pulsar.registerTransformer(registration.name, registration.transform)
+    } else if (registration.registrationType === 'payload') {
+      Pulsar.registerPayload(registration.name, registration.obj)
     }
+  }
 }
