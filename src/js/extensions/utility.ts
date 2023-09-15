f87c32d3aac9e4a422f929803d0a25003c836654
LeVan Smith <levictorsmith@gmail.com>
2023-09-15 10:01:39 -0700

    [CDS-73] Create Component Tokens (#8)
    * Generate output paths
    
    * Add build task for running exporter
    
    * Add ComponentTokens generator
    
    * Refactor token generators for consistency

--- src/js/extensions/utility.ts
+++ src/js/extensions/utility.ts
@@ -1,4 +1,4 @@
-import { FontToken, FontTokenValue } from '@supernovaio/supernova-sdk'
+import { FontToken, Token } from '@supernovaio/supernova-sdk'
 import {
   createPulsarFunction,
   createPulsarTransformer,
@@ -32,17 +32,84 @@ export const ternary = createPulsarFunction(
 )
 
 type FamilyGroups = {
-  [key: string]: FontTokenValue[]
+  [key: string]: FontToken[]
+}
+
+type ComponentGroups = {
+  [component: string]: Token[]
+}
+
+type ComponentProperty = {
+  component: string
 }
 
 export const groupByFamily = createPulsarTransformer(
   'groupByFamily',
   (array: FontToken[]) =>
-    array
-      .map((value) => value.value)
-      .reduce((acc, value) => {
-        acc[value.family] = acc[value.family] || []
-        acc[value.family].push(value)
-        return acc
-      }, {} as FamilyGroups),
+    array.reduce((acc, value) => {
+      acc[value.value.family] = acc[value.value.family] || []
+      acc[value.value.family].push(value)
+      return acc
+    }, {} as FamilyGroups),
+)
+
+export const groupByComponent = createPulsarFunction(
+  'groupByComponent',
+  (array: Token[]) => {
+    const componentOptions =
+      array.at(0)?.properties?.find((prop) => prop.codeName === 'component')
+        ?.options ?? []
+    return array.reduce((acc, value) => {
+      const optionId =
+        (value.propertyValues as ComponentProperty).component ?? ''
+      const componentName =
+        componentOptions.find((option) => option.id === optionId)?.name ?? ''
+      acc[componentName] = acc[componentName] || []
+      acc[componentName].push(value)
+      return acc
+    }, {} as ComponentGroups)
+  },
+)
+
+export const toJson = createPulsarFunction('toJson', (any: any) =>
+  JSON.stringify(any, null, '  '),
+)
+
+export const getFullTokenName = createPulsarFunction(
+  'getFullTokenName',
+  (token: Token) => {
+    return token.parent?.path?.concat(token.parent.name, token.name)?.join(' ')
+  },
+)
+
+export const getComponentTokenName = createPulsarFunction(
+  'getComponentTokenName',
+  (token: Token) =>
+    token.parent?.path
+      ?.slice(3)
+      ?.concat(token.parent.name, token.name)
+      ?.join(' '),
+)
+
+export const getTokenType = createPulsarFunction(
+  'getTokenType',
+  (token: Token) => token.tokenType,
+)
+
+export const getPackageName = createPulsarFunction(
+  'getPackageName',
+  (asPath: boolean = false, prefix: string = '', suffix: string = '') =>
+    (prefix ? prefix.split(/[./]/) : [])
+      .concat(Pulsar.exportConfiguration.packageName.split('.'))
+      .concat(suffix ? suffix.split(/[./]/) : [])
+      .join(asPath ? '/' : '.'),
+)
+
+export const filterComponentTokens = createPulsarTransformer(
+  'filterComponentTokens',
+  (tokens: Token[]) =>
+    tokens.filter(
+      (token) =>
+        (token.propertyValues as ComponentProperty).component === undefined,
+    ),
 )
